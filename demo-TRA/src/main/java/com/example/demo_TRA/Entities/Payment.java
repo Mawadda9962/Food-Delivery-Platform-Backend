7b45fa04075e482c1984c035347d29e244a8fb85
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-21 11:58:49 +0400

    Adding Payment Entity with mapping

--- demo-TRA/src/main/java/com/example/demo_TRA/Entities/Payment.java
+++ demo-TRA/src/main/java/com/example/demo_TRA/Entities/Payment.java
@@ -1,4 +1,16 @@
 package com.example.demo_TRA.Entities;
 
-public class Payment {
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+@Entity
+public class Payment extends BaseEntity {
+    public String paymentMethod;
+    public String status;
+
 }
