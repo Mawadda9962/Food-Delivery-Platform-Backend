a997085563ccabf7ad634169192eb5d09b84c15a
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-21 11:08:02 +0400

    Adding MenuItem Entity with mapping

---
+++ demo-TRA/src/main/java/com/example/demo_TRA/Entities/ComboMeal.java
@@ -0,0 +1,13 @@
+package com.example.demo_TRA.Entities;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+@Entity
+public class ComboMeal extends BaseEntity{
+}
