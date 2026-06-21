3e89e94c7e8ccac44fd859048806438af9dda138
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-21 11:00:01 +0400

    Adding MenuItem Entity with mapping

--- demo-TRA/src/main/java/com/example/demo_TRA/Entities/MenuItem.java
+++ demo-TRA/src/main/java/com/example/demo_TRA/Entities/MenuItem.java
@@ -1,4 +1,17 @@
 package com.example.demo_TRA.Entities;
 
-public class MenuItem {
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+@Entity
+public class MenuItem extends BaseEntity{
+    private String name;
+    private String description;
+    private Double price;
+    private
 }
