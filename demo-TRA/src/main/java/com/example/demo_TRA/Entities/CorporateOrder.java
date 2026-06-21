9cb8fa7e9fd2e1ddd5632543f028881fb3081552
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-21 11:22:11 +0400

    Adding CorporateOrder Entity with mapping

--- demo-TRA/src/main/java/com/example/demo_TRA/Entities/CorporateOrder.java
+++ demo-TRA/src/main/java/com/example/demo_TRA/Entities/CorporateOrder.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 @AllArgsConstructor
 @Data
 @NoArgsConstructor
@@ -12,6 +14,9 @@ import lombok.NoArgsConstructor;
 public class CorporateOrder extends BaseEntity{
     private String corporateCode;
     private String companyName;
+    private String costCenter;
+    private LocalDate orderDate;
+
 
 
 
