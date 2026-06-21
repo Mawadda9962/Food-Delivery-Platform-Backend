963f836f3faae1d24c75fe14d399448ba7c7935a
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-21 09:13:08 +0400

    initialize Spring Boot project structure

---
+++ demo-TRA/src/main/java/com/example/demo_TRA/DemoTraApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_TRA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTraApplication.class, args);
+	}
+
+}
