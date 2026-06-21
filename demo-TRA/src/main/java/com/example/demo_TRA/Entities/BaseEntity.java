38b35b24bed2da6a9ad8bc801ce662f17001bbe8
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-21 09:51:32 +0400

    Creating BaseEntity

--- demo-TRA/src/main/java/com/example/demo_TRA/Entities/BaseEntity.java
+++ demo-TRA/src/main/java/com/example/demo_TRA/Entities/BaseEntity.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @AllArgsConstructor
 @Data
 @NoArgsConstructor
@@ -12,5 +14,6 @@ import lombok.NoArgsConstructor;
 public class BaseEntity {
 
     private Integer id;
+    private LocalDateTime createDate;
 
 }
