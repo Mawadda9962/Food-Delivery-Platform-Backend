4f25e96901df01e6d8537b869bbb290018e7b8e6
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-21 09:49:24 +0400

    Creating BaseEntity

--- demo-TRA/src/main/java/com/example/demo_TRA/Entities/BaseEntity.java
+++ demo-TRA/src/main/java/com/example/demo_TRA/Entities/BaseEntity.java
@@ -1,4 +1,16 @@
 package com.example.demo_TRA.Entities;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+@Entity
 public class BaseEntity {
+
+    private Integer id;
+
 }
