7960fb641b2a5ef79c354a8106d75dbd9b46b495
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-21 09:53:24 +0400

    Creating BaseEntity

--- demo-TRA/src/main/java/com/example/demo_TRA/Entities/BaseEntity.java
+++ demo-TRA/src/main/java/com/example/demo_TRA/Entities/BaseEntity.java
@@ -1,6 +1,9 @@
 package com.example.demo_TRA.Entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,9 +16,13 @@ import java.time.LocalDateTime;
 @Entity
 public class BaseEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private LocalDateTime createDate;
     private LocalDateTime updateDate;
+    private Boolean isActive;
+
 
 
 }
