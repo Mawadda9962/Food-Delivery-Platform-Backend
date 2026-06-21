044295af94fe82a1cfe45ab640e13a572da82b9c
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-21 09:36:19 +0400

    Adding Customer Entity with mapping

--- demo-TRA/src/main/java/com/example/demo_TRA/Entities/Customer.java
+++ demo-TRA/src/main/java/com/example/demo_TRA/Entities/Customer.java
@@ -1,21 +1,35 @@
 package com.example.demo_TRA.Entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @AllArgsConstructor
 @Data
 @NoArgsConstructor
 @Entity
 public class Customer {
+
     private String firstName;
     private String lastName;
     private String email;
     private String phone;
-    private Integer passwordHash;
-    private String loyaltyPoints;
-    private Integer customerCode;
+    private String passwordHash;
+    private Integer loyaltyPoints;
+    private String customerCode;
+
+
+    @OneToMany
+    private List<CustomerAddress> addresses;
+
+    @OneToMany
+    private
+
+
+
 
 }
