065d5878670ff1194b0dae73a64d36a4b0d0bd34
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-21 09:25:07 +0400

    Adding Customer Entity with mapping

--- demo-TRA/src/main/java/com/example/demo_TRA/Entities/Customer.java
+++ demo-TRA/src/main/java/com/example/demo_TRA/Entities/Customer.java
@@ -2,5 +2,9 @@ package com.example.demo_TRA.Entities;
 
 public class Customer {
     private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+
 
 }
