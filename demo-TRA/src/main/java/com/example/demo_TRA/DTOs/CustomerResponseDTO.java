08a03078f97c91d0dfa617ffa57a6f350c1d30e6
Mawadda <moodalshukaili99@gmail.com>
2026-06-22 20:05:55 +0400

    Implementing Customer Summary DTO

--- demo-TRA/src/main/java/com/example/demo_TRA/DTOs/CustomerResponseDTO.java
+++ demo-TRA/src/main/java/com/example/demo_TRA/DTOs/CustomerResponseDTO.java
@@ -20,6 +20,12 @@ public class CustomerResponseDTO {
     public static CustomerResponseDTO fromEntity(Customer customer){
         CustomerResponseDTO dto = new CustomerResponseDTO();
 
+        customer.setFirstName(customer.getFirstName());
+        customer.setLastName(customer.getLastName());
+        customer.setCustomerEmail(customer.getCustomerEmail());
+        customer.setPhone(customer.getPhone());
+        customer.setCustomerCode(customer.getCustomerCode());
+        customer.setLoyaltyPoints(customer.getLoyaltyPoints());
+        return dto;
     }
-
 }
--- demo-TRA/src/main/java/com/example/demo_TRA/DTOs/CustomerSummaryDTO.java
+++ demo-TRA/src/main/java/com/example/demo_TRA/DTOs/CustomerSummaryDTO.java
@@ -1,4 +1,17 @@
 package com.example.demo_TRA.DTOs;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CustomerSummaryDTO {
+
+    private String firstName;
+    private String lastName;
+    private String customerCode;
+
+    public
 }
