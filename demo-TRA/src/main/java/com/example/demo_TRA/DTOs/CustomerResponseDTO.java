435bc209688a8ec94d52a3a4cef475a37756c9fa
Mawadda <moodalshukaili99@gmail.com>
2026-06-22 20:01:07 +0400

    Implementing Customer Response DTO

--- demo-TRA/src/main/java/com/example/demo_TRA/DTOs/CustomerResponseDTO.java
+++ demo-TRA/src/main/java/com/example/demo_TRA/DTOs/CustomerResponseDTO.java
@@ -1,5 +1,6 @@
 package com.example.demo_TRA.DTOs;
 
+import com.example.demo_TRA.Entities.Customer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,5 +9,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CustomerResponseDTO {
+    private String firstName;
+    private String lastName;
+    private String customerEmail;
+    private String phone;
+    private Integer loyaltyPoints;
+    private String customerCode;
+
+
+    public static CustomerResponseDTO fromEntity(Customer customer){
+        CustomerResponseDTO dto = new CustomerResponseDTO();
+
+    }
 
 }
