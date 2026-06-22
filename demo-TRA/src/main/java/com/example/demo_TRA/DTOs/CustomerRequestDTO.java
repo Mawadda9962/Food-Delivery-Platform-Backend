9a5bf1c9639ca1d2da16454c0e07eda09b9404b4
Mawadda <moodalshukaili99@gmail.com>
2026-06-22 18:18:19 +0400

    Implementing Customer Request DTO, and Configuring field validation

--- demo-TRA/src/main/java/com/example/demo_TRA/DTOs/CustomerRequestDTO.java
+++ demo-TRA/src/main/java/com/example/demo_TRA/DTOs/CustomerRequestDTO.java
@@ -1,5 +1,7 @@
 package com.example.demo_TRA.DTOs;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -7,9 +9,17 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CustomerRequestDTO {
 
+    @NotBlank(message = "First name is required")
     private String firstName;
+
+    @NotBlank(message = "Last name is required")
     private String lastName;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Please provide a valid email address")
     private String customerEmail;
+
+    @NotBlank(message = "Phone number is required")
     private String phone;
     private Integer loyaltyPoints;
     private String customerCode;
