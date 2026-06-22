7bbfab4efa6653258e9d540fea45e092ca0e8947
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-22 09:10:44 +0400

    Adding Customer Repositories

--- demo-TRA/src/main/java/com/example/demo_TRA/Repositories/CustomerRepository.java
+++ demo-TRA/src/main/java/com/example/demo_TRA/Repositories/CustomerRepository.java
@@ -2,11 +2,23 @@ package com.example.demo_TRA.Repositories;
 
 import com.example.demo_TRA.Entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 
-public interface CustomerRepository extends JpaRepository<Customer,Integer> {
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 
+    @Query("SELECT c FROM Customer c" + " WHERE c.email = :email AND c.isActive = true")
     List<Customer> findByEmail(@Param("Email") String email);
+
+
+    @Query("SELECT c FROM Customer c " + "WHERE c.loyaltyPoints >= :points AND c.isActive = true")
+    List<Customer> findByLoyaltyPointsGreaterThanEqual(@Param("points") int points);
+
+
+    @Query("SELECT c FROM Customer c " + "WHERE c.createdDate BETWEEN :start AND :end AND c.isActive = true")
+    List<Customer> findCustomersRegisteredBetween(@Param("start") LocalDate start, @Param("end") LocalDate end);
+
 }
