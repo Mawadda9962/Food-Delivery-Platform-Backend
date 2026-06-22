a9be9f719e1d98f1923b2d3ea5d8c5b2aebf23d3
Mawadda <moodalshukaili99@gmail.com>
2026-06-22 20:33:56 +0400

    Adding CustomerAddressRequestDTO

--- demo-TRA/src/main/java/com/example/demo_TRA/DTOs/CustomerAddressRequestDTO.java
+++ demo-TRA/src/main/java/com/example/demo_TRA/DTOs/CustomerAddressRequestDTO.java
@@ -1,4 +1,40 @@
 package com.example.demo_TRA.DTOs;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class CustomerAddressRequestDTO {
+
+    @NotBlank(message = "Street is required")
+    private String street;
+
+    @NotBlank(message = "City is required")
+    private String city;
+
+    @NotBlank(message = "Building description is required")
+    private String building;
+
+    private Boolean isDefault;
+
+
+    public CustomerAddress toEntity() { //For Creating
+        CustomerAddress customerAddress = new CustomerAddress();
+
+        customerAddress.setStreet(street);
+        customerAddress.setCity(city);
+        customerAddress.setBuilding(building);
+        customerAddress.setIsDefault(isDefault);
+
+        return customerAddress;
+    }
+
+    public void applyTo(CustomerAddress customerAddress) { //For Updating
+        customerAddress.setStreet(street);
+        customerAddress.setCity(city);
+        customerAddress.setBuilding(building);
+        customerAddress.setIsDefault(isDefault);
+    }
 }
