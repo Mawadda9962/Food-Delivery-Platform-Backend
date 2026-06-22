7b26acb1e716555395ded329022677d4be33d541
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-22 09:30:29 +0400

    Adding ComboMeal Repositories

--- demo-TRA/src/main/java/com/example/demo_TRA/Repositories/ComboMealRepository.java
+++ demo-TRA/src/main/java/com/example/demo_TRA/Repositories/ComboMealRepository.java
@@ -10,6 +10,9 @@ import java.util.List;
 
 public interface ComboMealRepository extends JpaRepository<ComboMeal,Integer> {
 
+    @Query("SELECT c FROM ComboMeal c " + "WHERE c.restaurant.id = :restaurantId AND c.isActive = true")
+    List<ComboMeal> findByRestaurantId(@Param("restaurantId") Integer restaurantId);
+
     @Query("SELECT c FROM ComboMeal c " + "JOIN c.menuItems m " + "WHERE m.id = :menuItemId AND c.isActive = true")
     List<ComboMeal> findCombosContainingMenuItem(@Param("menuItemId") Integer menuItemId);
 }
