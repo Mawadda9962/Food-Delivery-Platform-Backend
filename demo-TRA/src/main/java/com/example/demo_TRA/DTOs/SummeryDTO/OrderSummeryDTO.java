885a8341de38112dc44d185b697db85ff57f8f49
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-23 10:52:04 +0400

    adding OrderSummeryDTO

--- demo-TRA/src/main/java/com/example/demo_TRA/DTOs/SummeryDTO/OrderSummeryDTO.java
+++ demo-TRA/src/main/java/com/example/demo_TRA/DTOs/SummeryDTO/OrderSummeryDTO.java
@@ -4,8 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderSummeryDTO {
+
+    private String orderCode;
+    private LocalDate orderDate;
+    private String status;
+    private Double totalAmount;
+
+
 }
