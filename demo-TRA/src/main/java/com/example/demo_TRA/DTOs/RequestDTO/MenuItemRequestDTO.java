1100ec036503ce54a8a347c16ebbda6618bf24fc
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-23 09:27:19 +0400

    add MenuItem Request DTO

--- demo-TRA/src/main/java/com/example/demo_TRA/DTOs/RequestDTO/MenuItemRequestDTO.java
+++ demo-TRA/src/main/java/com/example/demo_TRA/DTOs/RequestDTO/MenuItemRequestDTO.java
@@ -6,4 +6,22 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class MenuItemRequestDTO {
+
+    @NotBlank(message = "Item name is required")
+    private String name;
+
+    @NotBlank(message = "Description is required")
+    private String description;
+
+    @NotNull(message = "Price is required")
+    @Min(value = 0, message = "Price cannot be negative")
+    private Double price;
+
+    private Boolean isAvailable;
+    private Boolean isVegetarian;
+    private Integer calories;
+
+    @NotNull(message = "Restaurant ID is required")
+    private Long restaurantId;
+
 }
