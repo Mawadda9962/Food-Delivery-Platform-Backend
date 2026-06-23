027fe13ca245e0137789bbe7f0ec2353477f047f
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-23 10:51:44 +0400

    adding OrderSummeryDTO

--- demo-TRA/src/main/java/com/example/demo_TRA/DTOs/SummeryDTO/OrderSummeryDTO.java
+++ demo-TRA/src/main/java/com/example/demo_TRA/DTOs/SummeryDTO/OrderSummeryDTO.java
@@ -1,4 +1,11 @@
 package com.example.demo_TRA.DTOs.SummeryDTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderSummeryDTO {
 }
