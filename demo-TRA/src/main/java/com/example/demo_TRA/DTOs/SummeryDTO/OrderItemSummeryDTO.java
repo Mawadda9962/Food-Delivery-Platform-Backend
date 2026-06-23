ab217d6db875c2d21392d60b743eba9c70721670
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-23 11:00:49 +0400

    adding OrderItemSummeryDTO

--- demo-TRA/src/main/java/com/example/demo_TRA/DTOs/SummeryDTO/OrderItemSummeryDTO.java
+++ demo-TRA/src/main/java/com/example/demo_TRA/DTOs/SummeryDTO/OrderItemSummeryDTO.java
@@ -1,4 +1,25 @@
 package com.example.demo_TRA.DTOs.SummeryDTO;
 
+import com.example.demo_TRA.Entities.OrderItem;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderItemSummeryDTO {
+    private Long id;
+    private Integer quantity;
+    private Double totalPrice;
+
+    public static OrderItemSummeryDTO fromEntity(OrderItem orderItem) {
+
+
+        OrderItemSummeryDTO dto = new OrderItemSummeryDTO();
+        dto.setQuantity(orderItem.getQuantity());
+        dto.setTotalPrice(orderItem.getTotalPrice());
+
+        return dto;
+    }
 }
