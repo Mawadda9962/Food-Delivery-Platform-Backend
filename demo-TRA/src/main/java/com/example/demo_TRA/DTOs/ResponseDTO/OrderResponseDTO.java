8ecf09e3b770d645508327bb8df3018c56c959f7
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-23 10:48:26 +0400

    adding OrderResponseDTO

--- demo-TRA/src/main/java/com/example/demo_TRA/DTOs/ResponseDTO/OrderResponseDTO.java
+++ demo-TRA/src/main/java/com/example/demo_TRA/DTOs/ResponseDTO/OrderResponseDTO.java
@@ -1,4 +1,23 @@
 package com.example.demo_TRA.DTOs.ResponseDTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderResponseDTO {
+
+    private String orderCode;
+    private LocalDate orderDate;
+    private String status;
+    private Double subtotal;
+    private Double deliveryFee;
+    private Double discountAmount;
+    private Double totalAmount;
+    private String deliveryNotes;
 }
+
