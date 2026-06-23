cb089ba4a784aa87264a17d4293a43abc72829ca
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-23 14:24:25 +0400

    Implementing GlobakExeptionHandiling

--- demo-TRA/src/main/java/com/example/demo_TRA/Exceptions/GlobalExceptionHandling.java
+++ demo-TRA/src/main/java/com/example/demo_TRA/Exceptions/GlobalExceptionHandling.java
@@ -1,7 +1,25 @@
 package com.example.demo_TRA.Exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 
 @RestControllerAdvice
 public class GlobalExceptionHandling {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleResourceNotFound(ResourceNotFoundException ex, WebRequest request){
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.NOT_FOUND,
+                HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                ex.getMessage(),
+                request.getDescription(false).replace("uri=", ""));
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
+
 }
