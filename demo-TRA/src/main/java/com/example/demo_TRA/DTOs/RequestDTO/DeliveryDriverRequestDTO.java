832910cafa1ac57f24650b73ad24f7b661321f2b
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-23 10:02:52 +0400

    adding DeliveryDriverRequestDTO

--- demo-TRA/src/main/java/com/example/demo_TRA/DTOs/RequestDTO/DeliveryDriverRequestDTO.java
+++ demo-TRA/src/main/java/com/example/demo_TRA/DTOs/RequestDTO/DeliveryDriverRequestDTO.java
@@ -1,4 +1,45 @@
 package com.example.demo_TRA.DTOs.RequestDTO;
 
+import com.example.demo_TRA.Entities.DeliveryDriver;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class DeliveryDriverRequestDTO {
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Phone number is required")
+    private String phone;
+
+    @NotBlank(message = "Vehicle type is required")
+    private String vehicleType;
+
+    private boolean isOnline;
+
+    public DeliveryDriver toEntity() { // For Creating
+        DeliveryDriver driver = new DeliveryDriver();
+
+        driver.setFirstName(firstName);
+        driver.setLastName(lastName);
+        driver.setEmail(email);
+        driver.setPhone(phone);
+        driver.setVehicleType(vehicleType);
+        driver.setOnline(isOnline);
+
+        return driver;
+    }
+
+
+
 }
