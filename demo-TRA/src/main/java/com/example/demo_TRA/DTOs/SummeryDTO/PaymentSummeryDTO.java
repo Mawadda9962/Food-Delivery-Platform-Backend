7a1256e3564be7da21f813cba19d522c11381e52
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-23 11:08:03 +0400

    adding RestaurantOwnerResponseDTO

--- demo-TRA/src/main/java/com/example/demo_TRA/DTOs/SummeryDTO/PaymentSummeryDTO.java
+++ demo-TRA/src/main/java/com/example/demo_TRA/DTOs/SummeryDTO/PaymentSummeryDTO.java
@@ -1,5 +1,6 @@
 package com.example.demo_TRA.DTOs.SummeryDTO;
 
+import com.example.demo_TRA.Entities.Payment;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,4 +9,20 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PaymentSummeryDTO {
+    private String status;
+    private Double amount;
+    private String transactionRef;
+
+    public static PaymentSummeryDTO fromEntity(Payment payment) {
+
+        PaymentSummeryDTO dto = new PaymentSummeryDTO();
+
+        dto.setStatus(payment.getStatus());
+        dto.setAmount(payment.getAmount());
+        dto.setTransactionRef(payment.getTransactionRef());
+
+        return dto;
+    }
+
+
 }
