bb6aa1efc13784138302f9323ace9987228b02d0
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-24 09:24:33 +0400

    Implementing Customer Service

--- demo-TRA/src/main/java/com/example/demo_TRA/Services/CustomerService.java
+++ demo-TRA/src/main/java/com/example/demo_TRA/Services/CustomerService.java
@@ -1,4 +1,28 @@
 package com.example.demo_TRA.Services;
 
+import com.example.demo_TRA.DTOs.RequestDTO.CustomerRequestDTO;
+import com.example.demo_TRA.DTOs.ResponseDTO.CustomerResponseDTO;
+import com.example.demo_TRA.Entities.Customer;
+import com.example.demo_TRA.Exceptions.DuplicateResourceException;
+import com.example.demo_TRA.Repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CustomerService {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    public CustomerResponseDTO customerResponseDTO(CustomerRequestDTO dto){
+        List<Customer> existingCustomers  = customerRepository.findByEmail(dto.getCustomerEmail());
+
+        if (!existingCustomers.isEmpty()){
+            throw new DuplicateResourceException("Customer with email" + dto.getCustomerEmail() + "already exists");
+        }
+
+
+    }
 }
