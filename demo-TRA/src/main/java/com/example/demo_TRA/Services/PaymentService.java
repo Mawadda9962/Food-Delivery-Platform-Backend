65399e7d2906286dca41bae0506a98fa76d04542
Mawadda <moodalshukaili99@gmail.com>
2026-06-24 21:08:44 +0400

    Adding PaymentRepository

--- demo-TRA/src/main/java/com/example/demo_TRA/Services/PaymentService.java
+++ demo-TRA/src/main/java/com/example/demo_TRA/Services/PaymentService.java
@@ -1,4 +1,11 @@
 package com.example.demo_TRA.Services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PaymentService {
+
+    @Autowired
+
 }
