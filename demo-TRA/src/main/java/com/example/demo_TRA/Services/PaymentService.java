227a14ef60de5048aa66319aa58998897d38a6d6
Mawadda <moodalshukaili99@gmail.com>
2026-06-24 23:44:05 +0400

    Adding PaymentService

--- demo-TRA/src/main/java/com/example/demo_TRA/Services/PaymentService.java
+++ demo-TRA/src/main/java/com/example/demo_TRA/Services/PaymentService.java
@@ -1,9 +1,19 @@
 package com.example.demo_TRA.Services;
 
+import com.example.demo_TRA.DTOs.ResponseDTO.PaymentResponseDTO;
+import com.example.demo_TRA.Entities.Order;
+import com.example.demo_TRA.Entities.Payment;
+import com.example.demo_TRA.Exceptions.InvalidOrderStateException;
+import com.example.demo_TRA.Exceptions.ResourceNotFoundException;
+import com.example.demo_TRA.Repositories.OrderRepository;
 import com.example.demo_TRA.Repositories.PaymentRepository;
 import com.example.demo_TRA.Repositories.ReviewRepository;
+import com.example.demo_TRA.Utils.HelperUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Service
 public class PaymentService {
@@ -12,6 +22,40 @@ public class PaymentService {
     PaymentRepository paymentRepository;
 
     @Autowired
-    ReviewRepository reviewRepository;
+    OrderRepository orderRepository;
+
+
+//processPayment (handle the transition of an order from a non-paid state to a secured, paid state)
+    public PaymentResponseDTO processPayment(Integer orderId, String method) {
+        Order order = orderRepository.findActiveById(orderId)
+                .orElseThrow(() -> new ResourceNotFoundException("Active Order not found with id: " + orderId));
+
+        if ("PAID".equalsIgnoreCase(order.getStatus())) {
+            throw new InvalidOrderStateException("Order has already been paid for.");
+        }
+
+        Payment payment = new Payment();
+        payment.setPaymentMethod(method.toUpperCase());
+        payment.setStatus("COMPLETED");
+        payment.setAmount(order.getTotalAmount());
+        payment.setTransactionRef(HelperUtils.generateCode("TXN", 10));
+        payment.setProcessedAt(LocalDateTime.now());
+        payment.setOrder(order);
+
+        payment.setCreateDate(LocalDate.now());
+        payment.setUpdateDate(LocalDateTime.now());
+        payment.setIsActive(true);
+
+        Payment savedPayment = paymentRepository.save(payment);
+
+        order.setStatus("PAID");
+        order.setUpdateDate(LocalDateTime.now());
+        orderRepository.save(order);
+
+        return PaymentResponseDTO.fromEntity(savedPayment);
+    }
+
+    //refund Payment
+
 
 }
