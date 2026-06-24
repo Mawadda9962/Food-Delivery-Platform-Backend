4b0fb7b7d6002666aa898a97b6bce9ff0d21dc6c
Mawadda <moodalshukaili99@gmail.com>
2026-06-24 20:10:40 +0400

    Implementing Delivery Service

--- demo-TRA/src/main/java/com/example/demo_TRA/Services/DeliveryService.java
+++ demo-TRA/src/main/java/com/example/demo_TRA/Services/DeliveryService.java
@@ -1,8 +1,21 @@
 package com.example.demo_TRA.Services;
 
+import com.example.demo_TRA.Repositories.DeliveryDriverRepository;
+import com.example.demo_TRA.Repositories.DeliveryRepository;
+import com.example.demo_TRA.Repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DeliveryService {
+    @Autowired
+    private DeliveryRepository deliveryRepository;
+
+    @Autowired
+    private DeliveryDriverRepository deliveryDriverRepository;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
 
 }
