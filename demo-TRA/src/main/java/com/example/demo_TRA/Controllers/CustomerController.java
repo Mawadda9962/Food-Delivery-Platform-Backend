df177b9b32167627cd6995772e1791f7917f83aa
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-06-25 10:07:05 +0400

    Implementing Customer Controller

--- demo-TRA/src/main/java/com/example/demo_TRA/Controllers/CustomerController.java
+++ demo-TRA/src/main/java/com/example/demo_TRA/Controllers/CustomerController.java
@@ -1,10 +1,14 @@
 package com.example.demo_TRA.Controllers;
 
+import com.example.demo_TRA.DTOs.RequestDTO.CustomerRequestDTO;
+import com.example.demo_TRA.DTOs.ResponseDTO.CustomerResponseDTO;
 import com.example.demo_TRA.Services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("customers")
@@ -16,5 +20,13 @@ public class CustomerController {
 
     //Create new customer
     @PostMapping
+    public ResponseEntity<CustomerResponseDTO> createCustomer(@RequestBody CustomerRequestDTO dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(customerService.createCustomer(dto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CustomerResponseDTO>> getAllCustomers(){
+        return ResponseEntity.ok(customerService.getAllCustomers());
+    }
 
 }
